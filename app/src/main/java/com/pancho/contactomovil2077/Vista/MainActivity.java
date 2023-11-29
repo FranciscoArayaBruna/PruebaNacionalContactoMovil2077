aed1ee36e9a80a363a8db5863f841acd14a5a408
FranciscoArayaBruna <143454124+FranciscoArayaBruna@users.noreply.github.com>
2023-11-29 18:58:11 -0300

    Primera carga (Diseño login y registro)

---
+++ app/src/main/java/com/pancho/contactomovil2077/Vista/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pancho.contactomovil2077.Vista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pancho.contactomovil2077.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/pancho/contactomovil2077/Vista/Usuario.java
@@ -0,0 +1,16 @@
+package com.pancho.contactomovil2077.Vista;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pancho.contactomovil2077.R;
+
+public class Usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario);
+    }
+}
